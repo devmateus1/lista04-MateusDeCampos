787078d85fac458e87560eab645f9460de7bd32a
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-19 19:20:47 -0300

    ex

--- exe01.py
+++ exe01.py
@@ -1,4 +1,4 @@
 #1-	Peça ao usuário para inserir, e em seguida exibir seu nome 3 vezes
-nome = input("Digie seu nome : ")
-for nome in range(3):
+nome = input("Digite seu nome : ")
+for i in range(3):
     print(nome)
